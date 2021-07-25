1295678b3a98b56a54a59b7e9aed1e5ca09e1ddf
chaicopadillag <chaicopadillag@gmail.com>
2021-07-25 16:39:26 -0500

    implementacion de enpoint para subir imagen a cloudinary

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const fs = require('fs');
 
+const cloudinary = require('cloudinary').v2
+
 const uploadFile = require("../helpers/uploadFile");
 const User = require("../models/user");
 const Product = require("../models/product");
 
+cloudinary.config(process.env.CLOUDINARY_URL);
 
 const uploadFilePhoto = async (req, res) => {
 
@@ -83,4 +86,42 @@ const showFileImageModel = async (req, res) => {
 }
 
 
-module.exports = { uploadFilePhoto, updateFilePhotoToModel, showFileImageModel }
+// TODO: cloudinary
+const updateFilePhotoToCloudinary = async (req, res) => {
+    const { collection, id } = req.params;
+
+    let model = null;
+    switch (collection) {
+        case 'user':
+            model = await User.findById(id);
+            break;
+        case 'product':
+            model = await Product.findById(id);
+            break;
+
+        default:
+            return res.status(401).json({ message: 'La collección no existe' });
+    }
+
+    if (model.imgUrl) {
+        const nameArray = model.imgUrl.split('/');
+        const nameFile = nameArray[nameArray.length - 1];
+        const [id_img] = nameFile.split('.');
+        cloudinary.uploader.destroy('dev/' + id_img);
+
+    }
+
+    const { tempFilePath } = req.files.file;
+
+    const { secure_url: photo_url } = await cloudinary.uploader.upload(tempFilePath, { folder: 'dev' });
+
+    model.imgUrl = photo_url;
+    model.save();
+
+    res.json({
+        message: 'Imagen actualizado',
+        model
+    })
+}
+
+module.exports = { uploadFilePhoto, updateFilePhotoToModel, showFileImageModel, updateFilePhotoToCloudinary }
--- routers/uploadsRouter.js
+++ routers/uploadsRouter.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
-const { uploadFilePhoto, updateFilePhotoToModel, showFileImageModel } = require('../controllers/uploadsController');
+const { uploadFilePhoto, updateFilePhotoToModel, showFileImageModel, updateFilePhotoToCloudinary } = require('../controllers/uploadsController');
 const { fileRequestValidation } = require('../middlewares/fieldsDbValidations/uploadFileValidation');
 const { uploadFileMiddleware, showImageMiddleware } = require('../middlewares/uploadFileMiddleware');
 const uploadsRouter = Router();
 
 uploadsRouter.post('/file', fileRequestValidation, uploadFilePhoto);
-uploadsRouter.put('/:collection/:id', uploadFileMiddleware, updateFilePhotoToModel);
+// uploadsRouter.put('/:collection/:id', uploadFileMiddleware, updateFilePhotoToModel);
+uploadsRouter.put('/:collection/:id', uploadFileMiddleware, updateFilePhotoToCloudinary);
 uploadsRouter.get('/img/:collection/:id', showImageMiddleware, showFileImageModel)
 
 module.exports = uploadsRouter;
